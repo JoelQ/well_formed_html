4164ad51d3398dbc3b00a5b75d2500dbd24da31f
Joel Quenneville <joel.quenneville@collegeplus.org>
2012-09-26 15:34:01 -0500

    write basic server

---
+++ server.rb
@@ -0,0 +1,20 @@
+require 'socket'
+
+server = TCPServer.new 2000 # Server bind to port 2000
+loop do
+  client = server.accept
+  request = client.gets
+
+  requested_file = request.match(/(?<=\/)\S*(?=\s)/)[0]
+  requested_file = 'index.html' if requested_file.empty?
+
+  begin
+    file = File.open requested_file, 'r'
+    client.print "HTTP/1.1 200/OK\r\nContent-type:text/html\r\n\r\n"
+    client.print file.read
+  rescue Errno::ENOENT
+    client.print "HTTP/1.1 404 Not Found\r\nContent-type:text/html\r\n\r\n"
+    client.print "<h1>Not Found</h1><p>The requested URL /#{requested_file} was not found on this server</p>"
+  end
+  client.close
+end
